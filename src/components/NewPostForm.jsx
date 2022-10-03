f2ea22903f332713951ece4227b0c7ab60d76b8d
Voronovich_dev <alexanderbizv1@gmail.com>
2022-10-03 17:53:39 +0300

    feat: add stylization

--- src/components/NewPostForm.jsx
+++ src/components/NewPostForm.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import s from './NewPostForm.module.scss'
 
 function NewPostForm({
     titleValue,
@@ -8,19 +9,26 @@ function NewPostForm({
     handleAction,
 }) {
     return (
-        <label>
-            <input
-                placeholder="Post title"
-                value={titleValue}
-                onChange={(e) => updateTitleText(e.target.value)}
-            />
-            <input
-                placeholder="Post body"
-                value={bodyValue}
-                onChange={(e) => updateBodyText(e.target.value)}
-            />
-            <button onClick={handleAction}>Add post</button>
-        </label>
+        <div>
+            <div className={s.form}>
+                <input
+                    placeholder="Post title"
+                    value={titleValue}
+                    onChange={(e) => updateTitleText(e.target.value)}
+                />
+                <input
+                    placeholder="Post body"
+                    value={bodyValue}
+                    onChange={(e) => updateBodyText(e.target.value)}
+                />
+                <button onClick={handleAction}>Add post</button>
+            </div>
+            <h5 className={s.warning}>
+                * due to the fact that jsonplaceholder always generates the same
+                key and I can&apos;t influence it - deleting one element you
+                created will delete the rest of the elements you created
+            </h5>
+        </div>
     )
 }
 
